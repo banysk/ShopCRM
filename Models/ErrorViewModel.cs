873bce2953255c8cdcf433276bed8cf9c4d4d2cd
Nikita Basov <49277174+banysk@users.noreply.github.com>
2022-05-30 02:26:24 +1000

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
